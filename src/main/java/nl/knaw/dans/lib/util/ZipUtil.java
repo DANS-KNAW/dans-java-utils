33231fe04f87ed7ced48de0a9a0cd5d1e1a3370b
Jan van Mansum <janvanmansum@gmail.com>
2026-02-18 13:55:31 +0100

    Fix resource leak in nl.knaw.dans.lib.util.ZipUtil#extractZipFile

--- src/main/java/nl/knaw/dans/lib/util/ZipUtil.java
+++ src/main/java/nl/knaw/dans/lib/util/ZipUtil.java
@@ -22,7 +22,6 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -46,7 +45,9 @@ public class ZipUtil {
                         return;
                     }
                     Files.createDirectories(outputPath.getParent()); // ZIPs created by Dataverse don't seem to comply with the ZIP spec, so we have to create the parent directories manually
-                    IOUtils.copy(input, Files.newOutputStream(outputPath.toFile().toPath()));
+                    try (var os = Files.newOutputStream(outputPath)) {
+                        IOUtils.copyLarge(input, os);
+                    }
                 }
                 catch (IOException e) {
                     throw new RuntimeException(e);
