2c3b5175fa34c6fe85725e99ed9042ea4667920c
Jan van Mansum <janvanmansum@gmail.com>
2026-04-18 19:16:46 +0200

    wip

--- src/main/java/nl/knaw/dans/lib/util/pollingtaskexec/ExecutorServiceTaskScheduler.java
+++ src/main/java/nl/knaw/dans/lib/util/pollingtaskexec/ExecutorServiceTaskScheduler.java
@@ -15,31 +15,20 @@
  */
 package nl.knaw.dans.lib.util.pollingtaskexec;
 
-import io.dropwizard.hibernate.UnitOfWorkAwareProxyFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.util.concurrent.ExecutorService;
 
 /**
  * A task scheduler implementation that schedules tasks for execution using an {@link ExecutorService}. This class allows for tasks to be executed asynchronously using the provided executor service.
- * If a {@link UnitOfWorkAwareProxyFactory} is provided, it will create a proxy for the task to ensure that it is executed within a unit of work context, which is useful for tasks that interact with a
- * database using Hibernate. If no proxy factory is provided, tasks will be executed directly without any additional context management.
  *
  */
 @RequiredArgsConstructor
 public class ExecutorServiceTaskScheduler implements TaskScheduler {
     private final ExecutorService executorService;
-    private final UnitOfWorkAwareProxyFactory unitOfWorkAwareProxyFactory;
-
-    public ExecutorServiceTaskScheduler(ExecutorService executorService) {
-        this(executorService, null);
-    }
 
     @Override
     public void schedule(Runnable task) {
-        if (unitOfWorkAwareProxyFactory != null) {
-            task = unitOfWorkAwareProxyFactory.create(Runnable.class, new Class[] { Runnable.class }, new Object[] { task });
-        }
         executorService.submit(task);
     }
 }
